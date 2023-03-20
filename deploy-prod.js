b4d282994743b4d1bca539650bba1c4f4df997ae
Naveen Rahul <100667256+naveenrahul97@users.noreply.github.com>
2023-03-20 11:53:30 +0530

    caches

--- deploy-prod.js
+++ deploy-prod.js
@@ -4,12 +4,12 @@ console.log('Publishing Please Wait...');
 const { parseAngularRoutes } = require('guess-parser');
 const routes = parseAngularRoutes('tsconfig.app.json');
 routes.filter(route=>route.path).forEach(route=>{
-if(!fs.existsSync(`dist/ud${route.path}`)){
-fs.mkdirSync(`dist/ud${route.path}`,{ recursive: true });
+if(!fs.existsSync(`dist/css_animation${route.path}`)){
+fs.mkdirSync(`dist/css_animation${route.path}`,{ recursive: true });
 }
-fs.copyFileSync('dist/ud/index.html',`dist/ud${route.path}/index.html`);
+fs.copyFileSync('dist/css_animation/index.html',`dist/css_animation${route.path}/index.html`);
 });
-ghpages.publish('./dist/ud',{
+ghpages.publish('./dist/css_animation',{
 message: 'Auto-generated commit'
 },function(){
 console.log('Successfully published! Have a nice day!');
